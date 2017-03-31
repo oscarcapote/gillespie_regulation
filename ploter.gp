3d0ff69ee5f9a25f42ec56c17bc68c474c662c82
Oscar Fajardo Fontiveros <Oscar Fajardo Fontiveros>
2017-03-31 06:09:02 +0200

    definitiu

--- ploter.gp
+++ ploter.gp
@@ -1,2 +1,27 @@
+set terminal png
+set output 'magnVSt.png'
+
+set ylabel 'num species'
+set xlabel 't'
 plot 'Results.dat' u 1:2 w l t 'P','' u 1:3 w l t 'M','' u 1:4 w l t 'DNA','' u 1:5 w l t 'PDNA'
-pause(-1)
+
+set terminal png
+set output 'M_freq.png'
+
+set ylabel 'num species'
+set xlabel 'M_st'
+plot 'hist_m.dat' u 1:2 w boxes t 'M'
+
+
+set terminal png
+set output 'P_freq.png'
+
+set ylabel 'freq'
+set xlabel 'P_st'
+plot 'hist_p.dat' u 1:2 w boxes t 'P' 
+
+set terminal png
+set output 'evo_promig.png'
+
+plot 'Results_repes.dat' u 1:2 w l t 'P','' u 1:3 w l t 'M','' u 1:4 w l t 'DNA',\
+      'Resultats_determinista.dat' u 1:2 w l t 'P','' u 1:3 w l t 'M','' u 1:4 w l t 'DNA'
--- treball_repes.f90
+++ treball_repes.f90
@@ -6,20 +6,35 @@ real(8) ::P,M,DNA,PDNA!Magnituds dinamiques
 real(8) :: tWait,tMax
 real(8) :: suma,a,ba
 real(8) :: t,tau,rnd,rnd2!Temps actual,temps estocastic i variable aleatoria entre 0 i 1
-integer(8) :: mu,i,MaxItt,itt
+integer(8) :: mu,i,MaxItt,itt,Nsimus,simu,Ntime,mesuresFetes
+real(8),dimension(:),allocatable :: timeVector,PVector,MVector,DNAVector
+real(8) :: dr!Delta_r
+
 
 !----------------------INICIALITZACIO---------------------
 include 'declaration.dec'
-t=0
-itt=1
-MaxItt=500
-tMax = 500
-tWait = 200
+MaxItt=1000000
+tMax = 300
+tWait = 0
+Nsimus = 300
+dr = 0.1d0
+Ntime = (tMax-tWait)/dr
+allocate(timeVector(Ntime))
+allocate(PVector(Ntime))
+allocate(MVector(Ntime))
+allocate(DNAVector(Ntime))
+timeVector = (/(tWait+i*dr, i=1,Ntime)/)
 !Parametres per reescalar U(0,1) i evitar que surti 0: Passa a ser U(a,1)
 a = 0.0000000001
 ba = 1.0d0-a
 !call srand(9)
+do simu=1,Nsimus
 !----------------------------------------------------------
+mesuresFetes = 0
+t=0
+itt=1
+include 'declaration.dec'
+Ntime = 1
 do
 !------------------UPDATE DE PROBABILITATS-----------------
 call prob_compute(Pr,a0)
@@ -38,12 +53,23 @@ enddo
 t = t+tau
 call reaction(mu)!Seleccionem reccio i q passi
 if(tWait.le.t)then!Si ha pasat es temps tWait imprimim resultats
-    print*,t,P,M,DNA,PDNA,Pr,mu
-    if(itt==MaxItt.or.t>tMax)then
-        stop
+    !print*,Ntime,t,itt!,P,M,DNA,PDNA,Pr,mu
+    mesuresFetes = mesuresFetes +1
+    if(t>timeVector(Ntime))then
+        PVector(Ntime) = PVector(Ntime) + P
+        MVector(Ntime) = MVector(Ntime) + M
+        DNAVector(Ntime) = DNAVector(Ntime) + DNA
+        Ntime = Ntime+1
+    endif
+    if(mesuresFetes==MaxItt.or.t>tMax)then
+        exit
     endif
     itt=itt+1
 endif
+enddo
+enddo
+do i=1,(tMax-tWait)/dr
+    print*,timeVector(i),PVector(i)/Nsimus,MVector(i)/Nsimus,DNAVector(i)/Nsimus
 enddo
 !----------------------------------------------------------
 contains
